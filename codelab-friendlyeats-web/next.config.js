3b0af3c52653d5057bcd87fd4b7e3eb27fd576a6
Dani <danutzviola40@gmail.com>
2025-02-11 07:24:03 +0200

    fix deploy #1

--- codelab-friendlyeats-web/next.config.js
+++ codelab-friendlyeats-web/next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	experimental: {
-		serverActions: true,
-	}
+	// Using stable configuration for Next.js 14
+	distDir: '.next',
+	swcMinify: true
 };
 
 module.exports = nextConfig;
